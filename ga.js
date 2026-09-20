637572a44f4c40be4379c23e2d897ed059840492
mayutarou53 <ykkmtmtykkmtmt@gmail.com>
2026-09-21 01:02:57 +0900

    Change GA_MEASUREMENT_ID for GA4 tracking
    Updated GA_MEASUREMENT_ID to new value.

--- ga.js
+++ ga.js
@@ -1,5 +1,5 @@
 // ★ここの「G-XXXXXXXXXX」を、GA4で発行された自分の測定IDに書き換える（この1か所だけ）
-var GA_MEASUREMENT_ID = 'G-15812020846';
+var GA_MEASUREMENT_ID = 'G-7Q3KQQ2JZQ';
 
 (function () {
   var s = document.createElement('script');
