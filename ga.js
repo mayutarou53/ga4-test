ba7bc410d8653de81232f7fd085757a0201aed41
mayutarou53 <ykkmtmtykkmtmt@gmail.com>
2026-09-21 00:25:28 +0900

    Add files via upload

---
+++ ga.js
@@ -0,0 +1,13 @@
+// ★ここの「G-XXXXXXXXXX」を、GA4で発行された自分の測定IDに書き換える（この1か所だけ）
+var GA_MEASUREMENT_ID = 'G-XXXXXXXXXX';
+
+(function () {
+  var s = document.createElement('script');
+  s.async = true;
+  s.src = 'https://www.googletagmanager.com/gtag/js?id=' + GA_MEASUREMENT_ID;
+  document.head.appendChild(s);
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = function () { window.dataLayer.push(arguments); };
+  window.gtag('js', new Date());
+  window.gtag('config', GA_MEASUREMENT_ID);
+})();
